9792301031c93a19b00eac5c44f72c3c360aefc3
Matias <mmunozs1996@gmail.com>
2024-12-19 22:33:51 -0300

    solucion error

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -10,19 +9,12 @@ def main():
     try:
         from django.core.management import execute_from_command_line
         execute_from_command_line(sys.argv)
-
-        
-        if "runserver" in sys.argv:
-            from scripts.populate import run
-            run()
-
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
 
 
 if __name__ == '__main__':
